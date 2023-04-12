3f6f614f49a22854676de4463347d96320dd2ecd
Anton Tsyrkunov <antontsyrkunov@gmail.com>
2023-04-12 18:18:41 +0300

    persist added

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from 'components/App';
 import './index.css';
-import { store } from '../src/redux/store';
+import { store, persistor } from '../src/redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename="/goit-react-hw-08-phonebook">
-    <Provider store={store}>
-      <App />
-    </Provider>
-   </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </PersistGate>
+  </BrowserRouter>
 );
